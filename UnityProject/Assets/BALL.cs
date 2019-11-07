715c4f856041ccb6b5b39e75b4743645ef36b0d3
lspss95108 <55518448+lspss95108@users.noreply.github.com>
2019-11-07 11:10:00 +0800

    期中

---
+++ UnityProject/Assets/BALL.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BALL : MonoBehaviour {
+    Rigidbody AAA;
+    [Header("速度")]
+    [Range(1, 270)]
+    public float speed = 1;
+    [Header("跳躍")]
+    [Range(1, 2000)]
+    public float jump = 1;
+    [Header("有沒有接觸地面")]
+    public bool isGround;
+
+
+    // Use this for initialization
+    void Start()
+    {
+        AAA = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+        Jump();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        isGround = true;
+       
+    }
+
+    private void Walk()
+    {
+        AAA.AddForce(new Vector3(speed * Input.GetAxis("Horizontal"), 0, 0));
+        AAA.AddForce(new Vector3(0, 0, speed * Input.GetAxis("Vertical")));
+
+    }
+
+    private void Jump()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && isGround == true)
+        {
+            isGround = false;
+            AAA.AddForce(new Vector3(0, jump, 0));
+        }
+
+    }
+}
---
+++ UnityProject/Assets/NPC.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    [Header("對話")]
+
+    public string Say = "";
+
+    public float SaySpeed;
+
+    [Header("任務")]
+
+    public bool Complete = false;
+
+    public int BateryFinish;
+
+    public int Batery;
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
